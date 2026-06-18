7eb87001982319d6c152e91d9a9da9a50b92ff75
Georg Pernice <uhrpq@student.kit.edu>
2026-06-18 17:53:51 +0200

    introduced errors when implementing a more accurate version of kugelschnitt

--- cg_exercise_02/02_whitted/src/exercise_02.cpp
+++ cg_exercise_02/02_whitted/src/exercise_02.cpp
@@ -30,25 +30,31 @@ bool intersect_sphere(
 {
     
     cg_assert(t);
-        cg_assert(std::fabs(glm::length(ray_direction) - 1.f) < EPSILON);
+    cg_assert(std::fabs(glm::length(ray_direction) - 1.f) < EPSILON);
         
     //-------------------Begin Georg Solution -------------------------------
     //intersection leads to need of solving at**2+bt+c=0 for t
     // with coefficients a, b, c being:
-    double a = glm::dot(ray_direction, ray_direction);
-    double b = 2 * glm::dot(ray_direction, ray_origin - center);
-    double c = glm::dot(ray_origin - center, ray_origin - center) - std::pow(radius, 2); 
+    float b = 2 * glm::dot(ray_direction, ray_origin - center);
+    float c = glm::dot(ray_origin - center, ray_origin - center) - std::pow(radius, 2); 
     //potentiall make synergies... TODO
     // analytically we get t= ( -b +- sqrt( b**2 - 4ac )) / 2a
-    double discriminant = b * b - 4*a*c;
+    float discriminant = b * b - 4*c;
     if (discriminant < 0.0) return false; // squareroot definitly complex -> no intersection
+
     //else we pierce the ball (neglect assume intersect in tangent style and assume two intersections :)
-    *t = (float)(-b - sqrt(discriminant)) / (2 * a) ; 
+    // we prefer t_small as the intersection that is closest to the camera blocks all others.
+    float t1 = (float)(-b - sqrt(discriminant)) / 2.f ; 
+    float t2 = (float)(-b + sqrt(discriminant)) / 2.f ; 
+    if (t2 * t1 < 0.f)
+        // if both solutions differ in sign choose the positive (visible) one
+        *t = ( t1 > 0.f) ? t2 : t1; 
+    else {
+        // if both carry same sign choose smaller one - later we throw away negative-t-solutions
+        *t = fmin( fabs(t1) , fabs(t2) );
+    }
+    // now throw away negative-t-solutions
     if (*t<0.f) return false;
-    // we only consider halbgerade
-    // case 1: squareroot counts negative --> ray shoots out of sphere --> t_small
-    // case 2: squareroot counts positive --> ray shoots into sphere --> t_big
-    // we prefer t_small for some reason. maybe the ray shoots to the user?!
     return true;
 //-------------------End Georg Solution ---------------------------------
 /*
@@ -224,7 +230,7 @@ glm::vec3 evaluate_reflection(
 {
 	// TODO: calculate reflective contribution by constructing and shooting a reflection ray.
         glm::vec3 R = reflect(V, N);
-        Ray ray(P+data.context.params.ray_epsilon*R,R); 
+        Ray ray(P + 10.f * data.context.params.ray_epsilon*R,R); 
         glm::vec3 contribution = trace_recursive(data, ray, depth);
 
 	return contribution;
@@ -245,7 +251,7 @@ glm::vec3 evaluate_transmission(
         
         if (refract(V, N, eta, &t)){
             // analogous to reflection
-            Ray ray(P+data.context.params.ray_epsilon*t,t); 
+            Ray ray(P + 10.f * data.context.params.ray_epsilon*t,t); 
             contribution = trace_recursive(data, ray, depth);
         }
         // if no refract we return zero
